83ade03690d1af55cb1555c3fee941c852510d4a
Nox <the0neWhoKnocks@users.noreply.github.com>
2018-11-10 12:55:52 -0800

    WP & refinements
    - Tuned the WP config to
      - Maintain chunk hashes based on content.
      - Display trimmed down stats.
    - Got more of the example fleshed out.
      - Generate `example`, `module`, and `vendor` bundles.

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,27 @@
 const { resolve } = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const SimpleProgressWebpackPlugin = require('simple-progress-webpack-plugin');
+
+const HASH_LENGTH = 5;
+const stats = {
+  chunks: false,
+  colors: true,
+  errors: true,
+  errorDetails: true,
+  modules: false,
+};
 
 module.exports = {
   devServer: {
     port: 3001,
+    stats: stats,
   },
   entry: {
     example: resolve(__dirname, './example/src/index.js'),
+    module: resolve(__dirname, './src/index.js'),
+    vendor: [],
   },
   module: {
     rules: [
@@ -18,25 +32,57 @@ module.exports = {
       },
     ],
   },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          chunks: 'initial',
+          test: resolve(__dirname, 'node_modules'),
+          name: 'vendor',
+          enforce: true,
+        },
+      },
+    },
+  },
+  output: {
+    path: resolve(__dirname, 'example/dist'),
+    publicPath: '/',
+    // assigns the hashed name to the file
+    filename: `[name]_[chunkhash:${ HASH_LENGTH }].js`,
+    // Point sourcemap entries to original disk location (format as URL on Windows)
+    devtoolModuleFilenameTemplate: info => resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+  },
   plugins: [
+    /**
+     * Gives more control of how bundles are hashed
+     */
+    new webpack.HashedModuleIdsPlugin({
+      hashDigestLength: HASH_LENGTH,
+    }),
+    /**
+     * Copies static assets over to dist
+     */
     new CopyWebpackPlugin([{
       from: resolve(__dirname, './example/src/imgs'),
       to: resolve(__dirname, './example/dist/imgs'),
       cache: true,
     }]),
+    /**
+     * Wires up the example HTML with generated bundles
+     */
     new HtmlWebpackPlugin({
       filename: 'index.html',
       template: resolve(__dirname, './example/src/index.html'),
     }),
+    /**
+     * Provides build progress in the CLI
+     */
+    new SimpleProgressWebpackPlugin({
+      format: 'minimal',
+    }),
   ],
-  output: {
-    path: resolve(__dirname, 'example/dist'),
-    filename: 'bundle.js',
-  },
   resolve: {
     extensions: ['.js'],
   },
-  stats: {
-    children: false,
-  },
+  stats: stats,
 };
